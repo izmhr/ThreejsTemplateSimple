6067ccc900101cb58aa69ab29caaa8ba8dc76afe
Atsushi Izumihara <atsizumi@gmail.com>
2015-04-12 21:46:08 +0900

    アンチエイリアス

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ function init() {
   scene = new THREE.Scene();
   camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 
-  renderer = new THREE.WebGLRenderer();
+  renderer = new THREE.WebGLRenderer({antialias: true});
   renderer.setSize(window.innerWidth, window.innerHeight);
   checkVersion();
   document.body.appendChild(renderer.domElement);
